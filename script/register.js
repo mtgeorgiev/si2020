1a650686f193ba0778cbd83bed30ebf710eb570a
mtgeorgiev <misho.takeda@gmail.com>
2020-05-12 18:58:43 +0300

    use js namespace design pattern

--- script/register.js
+++ script/register.js
@@ -1,30 +1,33 @@
-const onFormSubmitted = event => {
-    event.preventDefault();
-    
-    const formElement = event.target;
-    
-    const formData = {
-        username: formElement.querySelector("input[name='username']").value,
-        password: formElement.querySelector("input[name='password']").value,
-    };
-    // validate formData;
-    
-    console.log(formData);
-    
-    fetch('./endpoints/register.php', {
-            method: 'POST',
-            body: JSON.stringify(formData),
-            // body: new FormData(formElement),
-        })
-        .then(response => response.json())
-        .then(response => {
-            if (response.success) {
-                
-            } else {
-                
-            }
-        });
+const register = {
+    onFormSubmitted: event => {
+        event.preventDefault();
+
+        const formElement = event.target;
+
+        const formData = {
+            username: formElement.querySelector("input[name='username']").value,
+            password: formElement.querySelector("input[name='password']").value,
+        };
+        // validate formData;
+        
+        console.log(formData);
+
+        fetch('./endpoints/register.php', {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                // body: new FormData(formElement),
+            })
+            .then(response => response.json())
+            .then(register.onSuccess);
+    },
+    onSuccess: response => {
+        console.log(response);
+        if (response.success) {
+            
+        } else {
+            
+        }
+    }
 };
 
-
-document.getElementById('register').addEventListener('submit', onFormSubmitted);
+document.getElementById('register').addEventListener('submit', register.onFormSubmitted);
